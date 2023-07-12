e3cff12fc1cdbd621cbfd711b8b03a81b2be5b34
mcfly722 <7221798@gmail.com>
2023-07-12 23:12:37 +0300

    site variable

--- pci_device_database/syncronize.ps1
+++ pci_device_database/syncronize.ps1
@@ -1,5 +1,9 @@
-$response = iwr https://www.pcilookup.com/api.php?action=search
+param(
+    $site = 'https://www.pcilookup.com/api.php?action=search'
+)
+
+$response = Invoke-WebRequest $site
 
 $response.Content > pci_device_database.json
 
-$database =  ConvertFrom-JSON (get-content pci_device_database.json)
+#$database = ConvertFrom-JSON (get-content pci_device_database.json)
